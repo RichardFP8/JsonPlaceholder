e6bfec136f0d642a9503e8e6bd32a0a272b49a7b
Richard Perez <richard.floresperez0@gmail.com>
2022-12-11 11:49:24 -0500

    Used the POST method to add data to a REST API

--- scripts/newTODO.js
+++ scripts/newTODO.js
@@ -3,9 +3,10 @@ window.onload = () => {
    const addDataBtn = document.getElementById("submitForm");
    addDataBtn.onclick = function (){
     let data = new FormData();
-    data.append("id", document.getElementById("idInput").value);
+    data.append("id", document.getElementById("userIdInput").value);
     data.append("title", document.getElementById("titleInput").value);
     data.append("confirmation", document.getElementById("confirmationInput").value);
+    console.log(data);
     fetch("https://jsonplaceholder.typicode.com/todos" , {
         method: "POST",
         body: data
